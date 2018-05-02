4a21ab2b38a37dfc9afba7ef0145d99315b1a04d
Yongkyu (laptop-linux) <jyg1124@gmail.com>
2018-05-02 11:00:51 -0500

    sd

--- ControlledTVQueue/experiments_linux/TVGG1PS/TVGG1PS_main.jl
+++ ControlledTVQueue/experiments_linux/TVGG1PS/TVGG1PS_main.jl
@@ -9,7 +9,7 @@ N = 10000
 
 control_set = ("PD","SR")
 target_set = (0.1, 1.0, 10.0)
-dist_set = ("EXP",)
+dist_set = (("ER","LN") , ("LN","ER"))
 γ_set = (0.1, 0.01, 0.001)
 T_set = (2000.0, 2000.0, 20000.0)
 
@@ -17,8 +17,8 @@ for control in control_set
     for dist in dist_set
         for target in target_set
             for i in 1:3
-                arrival = dist
-                service = dist
+                arrival = dist[1]
+                service = dist[2]
                 coeff = (1.0, 0.2, γ_set[i])
                 T = T_set[i]
                 record = Record()
